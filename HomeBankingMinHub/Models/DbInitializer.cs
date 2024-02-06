67f1c680b0ba4641d955d8ae671c710da4e27ac2
MatiasSanPietro <matisanpietro98@gmail.com>
2024-02-06 10:17:26 -0300

    ClientLoan and Loan entities, migration, dbinitializer data

--- HomeBankingMinHub/Models/DbInitializer.cs
+++ HomeBankingMinHub/Models/DbInitializer.cs
@@ -2,20 +2,20 @@
 {
     public class DbInitializer
     {
-    public static void Initialize(HomeBankingContext context)
-    {
-        if (!context.Clients.Any())
+        public static void Initialize(HomeBankingContext context)
         {
-            var clients = new Client[]
+            if (!context.Clients.Any())
             {
+                var clients = new Client[]
+                {
                     new Client { Email = "vcoronado@gmail.com", FirstName="Victor", LastName="Coronado", Password="123456"},
                     new Client { Email = "vcoronado1@gmail.com", FirstName="Victor1", LastName="Coronado1", Password="1234561"},
                     new Client { Email = "vcoronado2@gmail.com", FirstName="Victor2", LastName="Coronado2", Password="1234562"}
-            };
+                };
 
-            context.Clients.AddRange(clients);
-            context.SaveChanges();
-        }
+                context.Clients.AddRange(clients);
+                context.SaveChanges();
+            }
             if (!context.Account.Any())
             {
                 var accountVictor = context.Clients.FirstOrDefault(c => c.Email == "vcoronado@gmail.com");
@@ -59,6 +59,72 @@
                     context.SaveChanges();
                 }
             }
+            if (!context.Loans.Any())
+            {
+                var loans = new Loan[]
+                {
+                    new Loan { Name = "Hipotecario", MaxAmount = 500000, Payments = "12,24,36,48,60" },
+                    new Loan { Name = "Personal", MaxAmount = 100000, Payments = "6,12,24" },
+                    new Loan { Name = "Automotriz", MaxAmount = 300000, Payments = "6,12,24,36" },
+                };
+
+                foreach (Loan loan in loans)
+                {
+                    context.Loans.Add(loan);
+                }
+
+                context.SaveChanges();
+
+                //clientloan (Prestamos del cliente)
+                var client1 = context.Clients.FirstOrDefault(c => c.Email == "vcoronado@gmail.com");
+                if (client1 != null)
+                {
+                    //3 tipos de prestamos
+                    var loan1 = context.Loans.FirstOrDefault(l => l.Name == "Hipotecario");
+                    if (loan1 != null)
+                    {
+                        var clientLoan1 = new ClientLoan
+                        {
+                            Amount = 400000,
+                            ClientId = client1.Id,
+                            LoanId = loan1.Id,
+                            Payments = "60"
+                        };
+                        context.ClientLoans.Add(clientLoan1);
+                    }
+
+                    var loan2 = context.Loans.FirstOrDefault(l => l.Name == "Personal");
+                    if (loan2 != null)
+                    {
+                        var clientLoan2 = new ClientLoan
+                        {
+                            Amount = 50000,
+                            ClientId = client1.Id,
+                            LoanId = loan2.Id,
+                            Payments = "12"
+                        };
+                        context.ClientLoans.Add(clientLoan2);
+                    }
+
+                    var loan3 = context.Loans.FirstOrDefault(l => l.Name == "Automotriz");
+                    if (loan3 != null)
+                    {
+                        var clientLoan3 = new ClientLoan
+                        {
+                            Amount = 100000,
+                            ClientId = client1.Id,
+                            LoanId = loan3.Id,
+                            Payments = "24"
+                        };
+                        context.ClientLoans.Add(clientLoan3);
+                    }
+
+                    context.SaveChanges();
+
+                }
+
+            }
+
         }
     }
 }
