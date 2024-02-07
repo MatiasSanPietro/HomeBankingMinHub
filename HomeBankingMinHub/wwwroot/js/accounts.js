ca7f4267a05cdebcf1d94c5a2da9ea684eb7f37b
MatiasSanPietro <matisanpietro98@gmail.com>
2024-02-07 08:55:39 -0300

    Front fix

--- HomeBankingMinHub/wwwroot/js/accounts.js
+++ HomeBankingMinHub/wwwroot/js/accounts.js
@@ -1,26 +1,27 @@
 var app = new Vue({
-    el:"#app",
-    data:{
-        clientInfo: {},
-        error: null
+  el: "#app",
+  data: {
+    clientInfo: {},
+    error: null,
+  },
+  methods: {
+    getData: function () {
+      axios
+        .get("/api/clients/1")
+        .then(function (response) {
+          //get client ifo
+          app.clientInfo = response.data;
+        })
+        .catch(function (error) {
+          // handle error
+          app.error = error;
+        });
     },
-    methods:{
-        getData: function(){
-            axios.get("/api/clients/1")
-            .then(function (response) {
-                //get client ifo
-                app.clientInfo = response.data;
-            })
-            .catch(function (error) {
-                // handle error
-                app.error = error;
-            })
-        },
-        formatDate: function(date){
-            return new Date(date).toLocaleDateString('en-gb');
-        }
+    formatDate: function (date) {
+      return new Date(date).toLocaleDateString("en-gb");
     },
-    mounted: function(){
-        this.getData();
-    }
-})
+  },
+  mounted: function () {
+    this.getData();
+  },
+});
