c372a72f338d9ea78de7c6e64d304ce981456e91
MatiasSanPietro <matisanpietro98@gmail.com>
2024-02-10 17:06:49 -0300

    Add GetAccountByClient method in accounts repositories

--- HomeBankingMinHub/Repositories/Interfaces/IAccountRepository.cs
+++ HomeBankingMinHub/Repositories/Interfaces/IAccountRepository.cs
@@ -7,5 +7,6 @@ namespace HomeBankingMinHub.Repositories.Interfaces
         IEnumerable<Account> GetAllAccounts();
         Account FindById(long id);
         void Save(Account account);
+        IEnumerable<Account> GetAccountsByClient(long clientId);
     }
 }
