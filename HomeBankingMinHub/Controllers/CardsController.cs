f4484f07e028f92ca1305a2a2f6aab92eda5982d
MatiasSanPietro <matisanpietro98@gmail.com>
2024-02-10 18:29:25 -0300

    Add CardsController & get clients current cards

---
+++ HomeBankingMinHub/Controllers/CardsController.cs
@@ -0,0 +1,58 @@
+﻿using HomeBankingMinHub.Models.DTOs;
+using HomeBankingMinHub.Models;
+using HomeBankingMinHub.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeBankingMinHub.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class CardsController : ControllerBase
+    {
+        private IClientRepository _clientRepository;
+
+        public CardsController(IClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        [HttpGet("clients/current/cards")]
+        public IActionResult GetCurrentCards()
+        {
+            try
+            {
+                string email = User.FindFirst("Client") != null ? User.FindFirst("Client").Value : string.Empty;
+                if (email == string.Empty)
+                {
+                    return Forbid();
+                }
+
+                Client client = _clientRepository.FindByEmail(email);
+
+                if (client == null)
+                {
+                    return Forbid();
+                }
+
+                var cardDTO = client.Cards.Select(c => new CardDTO
+                {
+                    Id = c.Id,
+                    CardHolder = c.CardHolder,
+                    Color = c.Color.ToString(),
+                    Cvv = c.Cvv,
+                    FromDate = c.FromDate,
+                    Number = c.Number,
+                    ThruDate = c.ThruDate,
+                    Type = c.Type.ToString()
+                }).ToList();
+
+                return Ok(cardDTO);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
